a87e2b4892d7fb2eefcfa75732af73434938cd71
Mark Hatch <markchatch@gmail.com>
2014-03-31 20:32:01 -0400

    Jumping related changes
    Added GroundCheck
    Added Jump Animation
    Created Jump blend tree based on vertical speed. (vSpeed).

---
+++ Assets/Resources/Scripts/PlayerControl.cs
@@ -0,0 +1,58 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerControl : MonoBehaviour {
+
+	public Animator anim;
+
+	public float maxSpeed = .01f;
+	public bool bGrounded = false;
+	public GameObject groundCheck;
+	float groundRadius = 0.2f;
+	public LayerMask whatIsGround;
+
+	public float jumpForce = 40f;
+
+	// Use this for initialization
+	void Start () {
+		anim = GetComponent<Animator> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (bGrounded && Input.GetButtonDown ("Jump")) {
+			anim.SetBool("Grounded", false);
+			jump ();
+		}
+	}
+
+	void FixedUpdate() {
+
+		bGrounded = Physics2D.OverlapCircle (groundCheck.transform.position, groundRadius, whatIsGround);
+		anim.SetBool ("Grounded", bGrounded);
+
+		//Move the character
+		float move = Input.GetAxis ("Horizontal");
+
+		anim.SetFloat ("Speed", Mathf.Abs (move));
+		anim.SetFloat ("vSpeed", rigidbody2D.velocity.y);
+
+		rigidbody2D.velocity = new Vector2 (move * maxSpeed/20, rigidbody2D.velocity.y);
+
+	}
+
+	void isGrounded()
+	{
+		bGrounded = true;
+	}
+
+	void isNotGrounded()
+	{
+		bGrounded = false;
+	}
+
+	void jump() {
+		anim.SetTrigger ("Jump");
+		rigidbody2D.AddForce (new Vector2(0f,jumpForce));
+	}
+}
