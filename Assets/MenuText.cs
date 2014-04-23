74c55335797169288197681b59f5c7ca52e9551e
Mark Hatch <markchatch@gmail.com>
2014-04-23 17:56:01 -0400

    Added MainMenu

---
+++ Assets/MenuText.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MenuText : MonoBehaviour {
+
+	public Renderer[] menuText;
+
+	// Use this for initialization
+	void Start () {
+		menuText = GetComponentsInChildren<Renderer> ();
+		foreach (Renderer r in menuText) {
+			r.sortingLayerID = 2;
+			r.sortingOrder = 3;
+			Debug.Log ("Sorting layer" + r.sortingLayerName);
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
