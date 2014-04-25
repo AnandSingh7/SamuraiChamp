ad1a40043cba901ffb16aa875c594da3fd2ed498
arvindeva <arvindeva@gmail.com>
2014-04-25 18:04:50 -0400

    add unfinished combat system

---
+++ Assets/Resources/Scripts/CombatSystem.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CombatSystem : MonoBehaviour {
+	public CharacterController controller;
+	public int health = 50;
+	public float knockbackX = 20F;
+	public float knockbackY = 1F;
+	
+	
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (health <= 0){
+			Application.LoadLevel("Main Menu");  
+		}
+	}
+	
+	void onControllerColliderHit(ControllerColliderHit hit){
+		if (hit.gameObject.tag == "Enemy"){
+			health -= 10;
+			controller.Move(new Vector2(-knockbackX, knockbackY));
+		}
+	}
+}
