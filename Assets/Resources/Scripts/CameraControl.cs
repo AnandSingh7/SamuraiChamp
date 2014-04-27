49e26758ffe0957276f75353e152534811cdc5c3
Mark Hatch <markchatch@gmail.com>
2014-04-26 20:59:04 -0400

    Separate Camera from Player

--- Assets/Resources/Scripts/CameraControl.cs
+++ Assets/Resources/Scripts/CameraControl.cs
@@ -3,13 +3,16 @@ using System.Collections;
 
 public class CameraControl : MonoBehaviour {
 
+	public GameObject player;
+	public Vector3 myPos;
+
 	// Use this for initialization
 	void Start () {
-	
+
 	}
 	
 	// Update is called once per frame
 	void Update () {
-
+		this.transform.position = player.transform.position + myPos;
 	}
 }
