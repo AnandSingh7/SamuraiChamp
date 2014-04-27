4872e0c1583b9d4ce27a6a5288ad46cba9f8c819
arvindeva <arvindeva@gmail.com>
2014-04-26 20:37:47 -0400

    added background music

--- Assets/Resources/Scripts/CameraControl.cs
+++ Assets/Resources/Scripts/CameraControl.cs
@@ -1,11 +1,13 @@
 ﻿using UnityEngine;
 using System.Collections;
 
+[RequireComponent (typeof (AudioSource))]
+
 public class CameraControl : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-	
+		audio.Play ();
 	}
 	
 	// Update is called once per frame
